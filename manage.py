b1ec4812f1ec3f72d802bc2bbdd234c1e92997ac
Abhimanyu Kolakotla <abhimanyu.kolakotla@in.ibm.com>
2020-10-04 16:34:40 +0530

    Missed manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vt_sample_python_app.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "feetracker.settings")
 
     from django.core.management import execute_from_command_line
 
