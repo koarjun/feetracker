8d9a12bd1088aaf59e418109b13704c4afffd141
koarjun <koarjun@users.noreply.github.com>
2020-10-05 00:03:12 +0530

    Update settings.py

--- feetracker/settings.py
+++ feetracker/settings.py
@@ -84,13 +84,22 @@ WSGI_APPLICATION = 'feetracker.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
+'''
 DATABASES = {
     "default": {
         "ENGINE" : "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3")
     }
 }
+'''
+DATABASES = {
+    "default": {
+        "ENGINE" : "django.db.backends.postgresql_psycopg2",
+        "NAME": os.path.join(BASE_DIR, "db.postgresql_psycopg2")
+    }
+}
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
