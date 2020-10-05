bb0686ae5a80cf4b4f10523fd1d4be5d5c8b3561
Abhimanyu Kolakotla <abhimanyu.kolakotla@in.ibm.com>
2020-10-05 14:46:45 +0530

    Modified with migrations

--- feetracker/settings.py
+++ feetracker/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import django_heroku
-
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -84,13 +84,26 @@ WSGI_APPLICATION = 'feetracker.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
+'''
 DATABASES = {
     "default": {
         "ENGINE" : "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3")
     }
 }
+'''
+DATABASES = {
+    "default": {
+        "ENGINE" : "django.db.backends.postgresql_psycopg2",
+        "NAME": os.path.join(BASE_DIR, "db.postgresql_psycopg2")
+    }
+}
+
+
+
+# DB Connection from heroku env.
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
